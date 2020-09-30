042c9840c08e83806f28d01dc18bba7c24b11532
harriet-the-spy <dpcairns@gmail.com>
2020-09-30 12:01:14 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # coin-flip-sept-2020
+
+Goal: Let the user choose heads or tails. Track their wins and losses (show as a percent)
+
+STATE: 
+- wins
+- total
+- _(losses are _derived state)_
+- _(current user selection -- radio button does this for us)_
+
+How do we SHOW this state?
+- display it in divs' textContent property
+
+HTML Elements we need to grab:
+- State changers:
+    - Coins (radio button)
+        - Why? To GET the user GUESS
+    - Submit button
+        - Why? To confirm the user guess
+- State viewers:
+    - win span
+        - show the wins
+    - loss span
+        - show the losses
+    - percent span
+        - show the percent
+
+How/when do we CHANGE this state?
+- when the user makes a selection and clicks the 'call it' button
+    - on click, what happens?
+        - 'flip the coin' and find out what side it landed on
+        - get the user's guess
+        - we determine if the user was right
+            - if they were right, increment the wins AND increment total
+            - if they were wrong, increment the total
--- app.js
+++ app.js
@@ -1,5 +1,52 @@
 // import functions and grab DOM elements
+const button = document.querySelector('#call-it');
+const winsSpan = document.querySelector('#wins');
+const percentSpan = document.querySelector('#percent');
+const resultSpan = document.querySelector('#result');
 
 // initialize state
+let wins = 0;
+let total = 0;
+
 
 // set event listeners to update state and DOM
+button.addEventListener('click', () => {
+    // - 'flip the coin' and find out what side it landed on
+    const randomNumber = Math.ceil(Math.random() * 2);
+
+    // i need to compare this random number to 'heads' or 'tails'
+
+    // we're comparing apples and oranges: two types. We need to convert the string to a number, or vice versa.
+
+    let itLandedOn;
+    // if randomNumber is 1, lets call it heads
+    // if it's 2, lets call it tails
+
+    if (randomNumber === 1) {
+        itLandedOn = 'heads';
+    } else {
+        itLandedOn = 'tails';
+    }
+
+    // - get the user's guess
+    // okay, which one is checked . . .
+    const checkedRadioButton = document.querySelector(':checked');
+    const userGuess = checkedRadioButton.value;
+
+    // - we determine if the user was right
+    if (userGuess === itLandedOn) {
+        //     - if they were right, increment the wins AND increment total
+        wins++;
+        total++;
+
+        winsSpan.textContent = wins;
+        percentSpan.textContent = `${(wins / total) * 100}%`;
+        resultSpan.textContent = 'You guessed right!';
+    } else {
+        //     - if they were wrong, increment just the total 
+        total++;
+
+        resultSpan.textContent = 'You guessed wrong!';
+        percentSpan.textContent = `${(wins / total) * 100}%`;
+    }
+});
